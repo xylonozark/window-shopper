9fc9878e3a42f637f79ec1213db04247c79be032
Xantes Fabela <xantesfabela@Xantess-iMac.local>
2017-10-13 13:00:43 -0500

    created a custom text field

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,31 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Xantes Fabela on 10/13/17.
+//  Copyright © 2017 Xantes Fabela. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+       super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2493311216)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+    
+    }
+
+
